8ea11120fc5db758f1c5f1cadefcce46e4877131
Roman Horshkov <roman.horshkov@gmail.com>
2025-08-01 17:09:39 +0200

    include utils

---
+++ app/src/utils/time_helper.c
@@ -0,0 +1,26 @@
+
+#define _POSIX_C_SOURCE 200112L
+#define _GNU_SOURCE
+#include "time_helper.h"
+
+#include <sys/time.h>
+#include <sys/timerfd.h> /* timerfd_create(), struct itimerspec */
+
+int time_helper_init(void)
+{
+    return timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+}
+
+int time_helper_set(int timer_fd, uint32_t s, uint32_t ns)
+{
+    struct itimerspec spec = {.it_value.tv_sec = s,
+                              .it_value.tv_nsec = ns,
+                              .it_interval.tv_sec = s,
+                              .it_interval.tv_nsec = ns};
+    return timerfd_settime(timer_fd, 0, &spec, NULL);
+}
+
+int time_helper_get_now(void)
+{
+    return (int)time(NULL);
+}
