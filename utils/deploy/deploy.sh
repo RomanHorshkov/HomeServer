e01fad6bc98a662231dca9baee4266f1f2da8cd9
Roman Horshkov <roman.horshkov@gmail.com>
2025-09-12 19:33:36 +0200

    deploy perfectly working

--- utils/deploy/deploy.sh
+++ utils/deploy/deploy.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Save current working directory
-ORIG_DIR="$(pwd)"
-
-proj="/home/roman/HomeServer"
-bin="server"
-dest="/srv/home_server/bin"
-
-# Go to the HomeServer root
-cd $proj
-
 # Define user and group name in a variable
 USER_GROUP_NAME="home_server"
+
+
+# Save current working directory
+ORIG_DIR="$(pwd)"
+USR="$(id -u -n)"
+PROJ_DIR="/home/$USR/HomeServer"
+bin="server"
+dest="/srv/home_server/bin"
+# Print info
+echo "Deploying HomeServer as user $USR from $ORIG_DIR"
+
+
+# Go to the HomeServer root
+cd $ORIG_DIR
+echo "pwd: $(pwd)"
 
 # Check for root privileges
 if [ "$EUID" -ne 0 ]; then
@@ -47,7 +52,7 @@ else
 fi
 
 # Check user's permissions on /srv
-echo "chown /srv/"$USER_GROUP_NAME" to "$USER_GROUP_NAME":"$USER_GROUP_NAME""
+echo "chown -R /srv/"$USER_GROUP_NAME" to "$USER_GROUP_NAME":"$USER_GROUP_NAME""
 chown -R "$USER_GROUP_NAME:$USER_GROUP_NAME" /srv/home_server
 echo "chmod -R 750 /srv/"$USER_GROUP_NAME""
 chmod -R 750 /srv/$USER_GROUP_NAME
@@ -70,4 +75,4 @@ install -D -m 0755 "build/bin/$bin" "$dest/$bin"
 echo "Starting $bin as $USER_GROUP_NAME on port 3491…"
 exec sudo -u "$USER_GROUP_NAME" -H \
   env HOME="/srv/home_server" \
-  bash -c "umask 027; cd /srv/home_server; exec '$dest/$bin' 3491 >> /srv/home_server/logs/server.log 2>&1"
+  bash -c "umask 027; cd /srv/home_server; exec '$dest/$bin' 3491 >> /srv/home_server/pri/server.log 2>&1"
