e7d6e348374abf72238c46901b970d2067ad7d09
Roman Horshkov <roman.horshkov@gmail.com>
2025-11-15 01:27:42 +0100

    move frontend to subproject

--- frontend/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p>© 2025 Roman Horshkov. All rights reserved.</p>
-      <p>Powered by pure POSIX/C11 | <a href="https://github.com/RomanHorshkov">GitHub</a></p>
-    </footer>
-  );
-}
